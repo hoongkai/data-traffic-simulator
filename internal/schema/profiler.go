dd0e1e979964d53cbba82e847533d05be66b1ab0
eric <hkchan.fl@gmail.com>
2026-07-18 01:40:19 +0800

    Added data reading and stats viewing for inferring schema

---
+++ internal/schema/profiler.go
@@ -0,0 +1,103 @@
+package schema
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ProfileCSV(path string, dataSchema Schema) (DatasetProfile, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return DatasetProfile{}, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	_, err = reader.Read()
+	if err != nil {
+		return DatasetProfile{}, err
+	}
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return DatasetProfile{}, err
+	}
+
+	profile := DatasetProfile{
+		RowCount: len(records),
+		Columns:  make([]ColumnProfile, len(dataSchema.Columns)),
+	}
+
+	for i, column := range dataSchema.Columns {
+		columnProfile := ColumnProfile{
+			Name:        column.Name,
+			Type:        column.Type,
+			Frequencies: make(map[string]int),
+		}
+
+		var sum float64
+		var numericCount int
+
+		for _, record := range records {
+			if i >= len(record) {
+				columnProfile.NullCount++
+				continue
+			}
+
+			value := strings.TrimSpace(record[i])
+
+			if value == "" {
+				columnProfile.NullCount++
+				continue
+			}
+
+			columnProfile.Count++
+			columnProfile.Frequencies[value]++
+
+			switch column.Type {
+			case Integer:
+				number, err := strconv.ParseFloat(value, 64)
+				if err == nil {
+					if numericCount == 0 || number < columnProfile.Min {
+						columnProfile.Min = number
+					}
+
+					if numericCount == 0 || number > columnProfile.Max {
+						columnProfile.Max = number
+					}
+
+					sum += number
+					numericCount++
+				}
+
+			case Float:
+				number, err := strconv.ParseFloat(value, 64)
+				if err == nil {
+					if numericCount == 0 || number < columnProfile.Min {
+						columnProfile.Min = number
+					}
+
+					if numericCount == 0 || number > columnProfile.Max {
+						columnProfile.Max = number
+					}
+
+					sum += number
+					numericCount++
+				}
+			}
+		}
+
+		columnProfile.UniqueCount = len(columnProfile.Frequencies)
+
+		if numericCount > 0 {
+			columnProfile.Mean = sum / float64(numericCount)
+		}
+
+		profile.Columns[i] = columnProfile
+	}
+
+	return profile, nil
+}
