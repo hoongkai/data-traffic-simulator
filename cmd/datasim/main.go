f9a6d1463ca2cce71b7c814dc5a0f1caaa3eacdb
eric <hkchan.fl@gmail.com>
2026-07-18 01:41:18 +0800

    Updated simulator for latest changes

--- cmd/datasim/main.go
+++ cmd/datasim/main.go
@@ -32,7 +32,6 @@ func main() {
 		},
 	}
 
-	// Added dataset reading for analyzer.go use
 	if dataset := os.Getenv("DATASET"); dataset != "" {
 		inferredSchema, err := schema.AnalyzeCSV(dataset)
 		if err != nil {
@@ -52,7 +51,42 @@ func main() {
 			)
 		}
 
-		fmt.Println()
+		profile, err := schema.ProfileCSV(dataset, dataSchema)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("\nDataset profile:\n")
+		fmt.Printf("  Rows: %d\n\n", profile.RowCount)
+
+		for _, column := range profile.Columns {
+			fmt.Printf("%s\n", column.Name)
+			fmt.Printf("  Type: %s\n", column.Type)
+			fmt.Printf("  Values: %d\n", column.Count)
+			fmt.Printf("  Nulls: %d\n", column.NullCount)
+			fmt.Printf("  Unique: %d\n", column.UniqueCount)
+
+			if column.Type == schema.Integer ||
+				column.Type == schema.Float {
+				fmt.Printf("  Min: %.4f\n", column.Min)
+				fmt.Printf("  Max: %.4f\n", column.Max)
+				fmt.Printf("  Mean: %.4f\n", column.Mean)
+			}
+
+			if column.Type == schema.String {
+				fmt.Println("  Frequencies:")
+
+				for value, count := range column.Frequencies {
+					fmt.Printf(
+						"    %s: %d\n",
+						value,
+						count,
+					)
+				}
+			}
+
+			fmt.Println()
+		}
 	}
 
 	g := generator.New(time.Now().UnixNano())
